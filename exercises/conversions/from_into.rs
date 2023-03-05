efb5e6e04dd40d4f5c80d4408536adce56f69086
astrobuddha <jozefleo@protonmail.com>
2023-03-05 18:13:51 -0500

    finsihed from into, str, and as

--- exercises/conversions/from_into.rs
+++ exercises/conversions/from_into.rs
@@ -35,10 +35,29 @@ impl Default for Person {
 // If while parsing the age, something goes wrong, then return the default of Person
 // Otherwise, then return an instantiated Person object with the results
 
-// I AM NOT DONE
-
 impl From<&str> for Person {
     fn from(s: &str) -> Person {
+        let parts: Vec<&str> = s.split(",").collect();
+
+        if parts.len() != 2 {
+            return Person::default();
+        }
+
+        let name = String::from(parts[0]);
+
+        if name.is_empty() {
+            return Person::default();
+        }
+
+        let age: usize = match parts[1].parse() {
+            Ok(n) => n,
+            Err(e) => {
+                eprintln!("Failed to parse age");
+                return Person::default();
+            }
+        };
+
+        Person { name, age }
     }
 }
 
